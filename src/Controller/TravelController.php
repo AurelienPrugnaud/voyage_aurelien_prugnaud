a474ef4af964a821c48e72bd7c50bc1e4b2e32a4
Aurélien Prugnaud <aurelienpru@gmail.com>
2021-11-20 17:33:12 +0100

    4th : add upload img/pdf file

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/travel")
@@ -29,13 +30,82 @@ class TravelController extends AbstractController
     /**
      * @Route("/new", name="travel_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $travel = new Travel();
         $form = $this->createForm(TravelType::class, $travel);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+			$pdf = $form->get('pdf')->getData();
+			if ($pdf) {
+				$originalFilename = pathinfo($pdf->getClientOriginalName(), PATHINFO_FILENAME);
+				// this is needed to safely include the file name as part of the URL
+				$safeFilename = $slugger->slug($originalFilename);
+				$newFilename = $safeFilename.'-'.uniqid().'.'.$pdf->guessExtension();
+
+				// Move the file to the directory where brochures are stored
+				try {
+					$pdf->move(
+						$this->getParameter('pdf_directory'),
+						$newFilename
+					);
+				} catch (FileException $e) {
+					// ... handle exception if something happens during file upload
+				}
+
+				// updates the 'brochureFilename' property to store the PDF file name
+				// instead of its contents
+				$travel->setPdf($newFilename);
+			}
+
+			$imgFile1 = $form->get('image1')->getData();
+			if ($imgFile1) {
+				$originalFilename = pathinfo($imgFile1->getClientOriginalName(), PATHINFO_FILENAME);
+				$safeFilename = $slugger->slug($originalFilename);
+				$newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile1->guessExtension();
+				try {
+					$imgFile1->move(
+						$this->getParameter('img_directory'),
+						$newFilename
+					);
+				} catch (FileException $e) {
+				}
+				$travel->setImage1($newFilename);
+			}
+
+			$imgFile2 = $form->get('image2')->getData();
+			if ($imgFile2) {
+				$originalFilename = pathinfo($imgFile2->getClientOriginalName(), PATHINFO_FILENAME);
+				$safeFilename = $slugger->slug($originalFilename);
+				$newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile2->guessExtension();
+				try {
+					$imgFile2->move(
+						$this->getParameter('img_directory'),
+						$newFilename
+					);
+				} catch (FileException $e) {
+				}
+				$travel->setImage2($newFilename);
+			}
+
+			$imgFile3 = $form->get('image3')->getData();
+			if ($imgFile3) {
+				$originalFilename = pathinfo($imgFile3->getClientOriginalName(), PATHINFO_FILENAME);
+				$safeFilename = $slugger->slug($originalFilename);
+				$newFilename = $safeFilename . '-' . uniqid() . '.' . $imgFile3->guessExtension();
+				try {
+					$imgFile3->move(
+						$this->getParameter('img_directory'),
+						$newFilename
+					);
+				} catch (FileException $e) {
+				}
+				$travel->setImage3($newFilename);
+			}
+
+
             $entityManager->persist($travel);
             $entityManager->flush();
 
